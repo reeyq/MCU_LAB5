16085db0f74df5e52b83de7612b8f23ecb8485e0
LAPTOP-B8FELC64\Canyon <canyonn1708@gmail.com>
2023-12-21 22:27:04 +0700

    lab5

---
+++ LAB5_MCU/Core/Inc/software_timer.h
@@ -0,0 +1,18 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Dec 21, 2023
+ *      Author: Canyon
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer0_flag;
+extern int timer1_flag;
+
+void setTimer0(int duration);
+void setTimer1(int duration);
+void timer_run();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
